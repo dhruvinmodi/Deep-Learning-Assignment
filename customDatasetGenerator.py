a6b6cb5aa58cd4e5304eb0ed684dd98dc3baf0c5
dhruvinmodi <dhruvinmodi2015@gmail.com>
2023-01-31 18:59:37 +0530

    Comments are added

--- customDatasetGenerator.py
+++ customDatasetGenerator.py
@@ -3,22 +3,29 @@ import numpy as np
 import random
 from sklearn.utils import shuffle
 
+# Reading MNIST dataset
 print('Reading MNIST dataset files...')
-train = pd.read_csv('./dataset/train.csv')
-trainX = train.iloc[:, 1:].copy().to_numpy()
-trainY = train.iloc[:, 0].to_numpy()
+train = pd.read_csv('./dataset/train.csv')  # 60k samples 785 dimention
+trainX = train.iloc[:, 1:].copy().to_numpy()  # 60k samples 784 dimention
+trainY = train.iloc[:, 0].to_numpy() # 60k samples 1 dimention
 
-trainX = trainX.reshape(60000, 28, 28)
+trainX = trainX.reshape(60000, 28, 28) # Reshaping samples
 
+'''
+    - trainX_pool is dictinary for numbers from 0 to 9.
+    - each key in dict is an array
+'''
 print('Creating trainX_pool...')
 trainX_pool = {
     0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []
 }
 
+# Filling dictionary
 for x,y in zip(trainX, trainY):
-    y_n, x_n = np.nonzero(x)
-    trainX_pool[y].append(x[np.min(y_n):np.max(y_n), np.min(x_n):np.max(x_n)])
+    y_n, x_n = np.nonzero(x) # selecting non zero pixels only
+    trainX_pool[y].append(x[np.min(y_n):np.max(y_n), np.min(x_n):np.max(x_n)]) # cropping image and appending to pool
 
+#loading given data 
 print('Reading Multidigit dataset files...')
 data0 = np.load('./dataset/data0.npy')
 lab0 = np.load('./dataset/lab0.npy')
@@ -29,18 +36,24 @@ lab1 = np.load('./dataset/lab1.npy')
 data2 = np.load('./dataset/data2.npy')
 lab2 = np.load('./dataset/lab2.npy')
 
+# concatinate all three file data
 x = np.concatenate((data0, data1, data2), axis=0)
 y = np.concatenate((lab0, lab1, lab2), axis=0)
 
-x,y = shuffle(x,y)
+x,y = shuffle(x,y) # shuffle given data
 
-x_test = x[:2000]
+x_test = x[:2000] # 2k rows are selected for testing purpose
 y_test = y[:2000]
-x_train = x[2000:]
+x_train = x[2000:] # remaining are for training 28k
 y_train = y[2000:]
 
 
-def generateMultidigitDataset(sampleCount=100, x=[], y=[]):
+def generateMultidigitDataset(sampleCount=10, x=[], y=[]):
+    '''
+    - This is a function to generate custom dataset which is similar to 
+    existing data.
+    - sampleCount: number of images will be sinthesized for same number
+    '''
     for i in range(10):
         for j in range(10):
             for k in range(10):
@@ -52,10 +65,10 @@ def generateMultidigitDataset(sampleCount=100, x=[], y=[]):
                                 t1 = np.zeros((40, 168))
                                 X_MIN = random.choice(range(5,15))
                                 Y_MIN = random.choice(range(5,15))
-                                img1 = trainX_pool[i][random.choice(range(len(trainX_pool[i])))].copy()
-                                img2 = trainX_pool[j][random.choice(range(len(trainX_pool[j])))].copy()
-                                img3 = trainX_pool[k][random.choice(range(len(trainX_pool[k])))].copy()
-                                img4 = trainX_pool[l][random.choice(range(len(trainX_pool[l])))].copy()
+                                img1 = trainX_pool[i][random.choice(range(len(trainX_pool[i])))].copy() # selecting random image from trainXpool for first number
+                                img2 = trainX_pool[j][random.choice(range(len(trainX_pool[j])))].copy() # selecting random image from trainXpool for second number
+                                img3 = trainX_pool[k][random.choice(range(len(trainX_pool[k])))].copy() # selecting random image from trainXpool for third number
+                                img4 = trainX_pool[l][random.choice(range(len(trainX_pool[l])))].copy() # selecting random image from trainXpool for fourth number
 
                                 t1[Y_MIN: Y_MIN + img1.shape[0], X_MIN: X_MIN + img1.shape[1]] = t1[Y_MIN: Y_MIN + img1.shape[0], X_MIN: X_MIN + img1.shape[1]] + img1
 
@@ -84,15 +97,19 @@ def generateMultidigitDataset(sampleCount=100, x=[], y=[]):
 
     return x,y
 
+# Generating custom dataset
 print('Generating Multidigit dataset...')
 multiDigitTrainX, multiDigitTrainY = generateMultidigitDataset(3, [], [])
 
+# concatinate new dataset and given dataset
 multiDigitTrainX = np.concatenate((multiDigitTrainX, x_train), axis=0)
 multiDigitTrainY = np.concatenate((multiDigitTrainY, y_train), axis=0)
 
+# Converting to numpy array and reshaping training data
 multiDigitTrainX = np.asarray(multiDigitTrainX).reshape(len(multiDigitTrainX), 40, 168, 1)
 multiDigitTrainY = np.asarray(multiDigitTrainY).reshape(len(multiDigitTrainY), 1)
 
+# Converting to numpy array and reshaping testing data
 multiDigitTestX = np.asarray(x_test).reshape(x_test.shape[0], 40, 168, 1)
 multiDigitTestY = np.asarray(y_test).reshape(y_test.shape[0], 1)
 
@@ -101,6 +118,7 @@ print('multiDigitTrainX: {} - multiDigitTrainY: {}'.format(multiDigitTrainX.shap
 print('multiDigitTestX: {} - multiDigitTestY: {}'.format(multiDigitTestX.shape, multiDigitTestY.shape))
 print('------------------')
 
+# saving dataset
 print('Saving dataset...')
 with open('./customDataset/trainX.npy', 'wb') as f:
     np.save(f, multiDigitTrainX)
